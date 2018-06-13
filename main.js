36918e566eeaa570cbbae165f8df997bf0d6c448
WarrenDavies <warren_davies81@hotmail.com>
2018-06-13 20:00:21 +0100

    removed race function from main draw

--- main.js
+++ main.js
@@ -4402,6 +4402,112 @@ do {
 	c.closePath();
 }	//draw headlights
 	
+	
+function updateRace() {
+	////race
+	if (race.completeTimer < 0.1) {
+	c.fillStyle = raceCheckPoints[race.currentCheckPoint].colour;
+	c.beginPath();
+	c.save();
+	c.fillRect(
+	raceCheckPoints[race.currentCheckPoint].x - cameraX, 
+	raceCheckPoints[race.currentCheckPoint].y - cameraY, 
+	raceCheckPoints[race.currentCheckPoint].w, 
+	raceCheckPoints[race.currentCheckPoint].h);
+	c.fill();
+	//console.log(raceCheckPoints[race.currentCheckPoint].x);
+	//console.log(raceCheckPoints[race.currentCheckPoint].y);
+
+	//console.log (raceCheckPoints[0].lines.top.p2x); 
+
+	race.collidesCheckPoint = false;
+	if (race.distaceToCheckPoint < 200) {
+		for (var vehicleLine in vehiclesOnScreen[Player1.mot].lines) {
+			for (var checkPointLine in raceCheckPoints[race.currentCheckPoint].lines) {			
+				if (testLines(
+					vehiclesOnScreen[Player1.mot].lines[vehicleLine].p1x, 
+					vehiclesOnScreen[Player1.mot].lines[vehicleLine].p1y, 
+					vehiclesOnScreen[Player1.mot].lines[vehicleLine].p2x, 
+					vehiclesOnScreen[Player1.mot].lines[vehicleLine].p2y, 
+					
+					raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p1x, 
+					raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p1y, 
+					raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p2x, 
+					raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p2y)) {
+					
+					race.collidesCheckPoint = true;
+				}
+			}	
+		}
+	}	
+
+	//if (collidesSpecify( Player1.x, Player1.y, Player1.w, Player1.h, raceCheckPoints[race.currentCheckPoint].x, raceCheckPoints[race.currentCheckPoint].y, raceCheckPoints[race.currentCheckPoint].w, raceCheckPoints[race.currentCheckPoint].h ) && race.completeTimer < 1 ) {
+
+	if (race.collidesCheckPoint === true) {	
+		
+		if (race.currentCheckPoint === 0) {
+			race.on = true;
+		}
+		console.log("checkpoint");
+		if (raceCheckPoints[race.currentCheckPoint].colour === "red") {
+			race.on = false;
+			race.currentCheckPoint = 0;
+			
+			
+			race.completeTimer = 60;
+		} else {
+			race.currentCheckPoint++;
+		}
+		
+		
+	}
+
+	//console.log(race.on);
+
+	if (race.on === true) {
+		
+		race.deltaX = (raceCheckPoints[race.currentCheckPoint].x + (raceCheckPoints[race.currentCheckPoint].w / 2)) - Player1.x;
+		race.deltaY = (raceCheckPoints[race.currentCheckPoint].y + (raceCheckPoints[race.currentCheckPoint].h / 2)) - Player1.y;
+		race.angle = Math.atan2(race.deltaY,  race.deltaX);
+		race.xVector = Math.cos(race.angle);
+		race.yVector = Math.sin(race.angle);
+		
+		
+		//race.distaceToCheckPoint = Math.sqrt(Math.pow(race.xVector * 100, 2) + Math.pow(race.yVector * 100, 2) )
+		
+		race.distaceToCheckPoint = Math.sqrt(Math.pow(race.deltaX, 2) + Math.pow(race.deltaY, 2) )
+		
+		console.log (race.distaceToCheckPoint);
+		
+		if (race.distaceToCheckPoint < 150) {
+			race.indicatorAdjustor = 150 - race.distaceToCheckPoint;
+		} else {
+			race.indicatorAdjustor = 0;
+		}
+		
+		race.indicatorX = race.xVector * (150 - race.indicatorAdjustor);
+		race.indicatorY = race.yVector * (150 - race.indicatorAdjustor);
+		
+		
+		
+		c.beginPath();
+		c.save();
+		c.fillStyle = raceCheckPoints[race.currentCheckPoint].colour;
+		c.translate(Player1.x, Player1.y);
+		c.arc(race.indicatorX - 5 - cameraX, race.indicatorY - 5 - cameraY, 5, 0, 2*Math.PI);
+		c.lineWidth = 2;
+		c.stroke();
+		c.fill();
+		c.restore();
+		
+		race.currentRaceTime += 0.02; 
+	}
+
+	}
+} // updateRace	
+	
+	
+	
  // game loop interval
  setInterval(mainDraw, INTERVAL);
 
@@ -4486,108 +4592,7 @@ updateTimers();
 
 /// STUFF NOT SPLIT OUT YET
 
-////race
-if (race.completeTimer < 0.1) {
-c.fillStyle = raceCheckPoints[race.currentCheckPoint].colour;
-c.beginPath();
-		c.save();
-
-		
-c.fillRect(
-raceCheckPoints[race.currentCheckPoint].x - cameraX, 
-raceCheckPoints[race.currentCheckPoint].y - cameraY, 
-raceCheckPoints[race.currentCheckPoint].w, 
-raceCheckPoints[race.currentCheckPoint].h);
-c.fill();
-//console.log(raceCheckPoints[race.currentCheckPoint].x);
-//console.log(raceCheckPoints[race.currentCheckPoint].y);
-
-//console.log (raceCheckPoints[0].lines.top.p2x); 
-
-race.collidesCheckPoint = false;
-if (race.distaceToCheckPoint < 200) {
-	for (var vehicleLine in vehiclesOnScreen[Player1.mot].lines) {
-		for (var checkPointLine in raceCheckPoints[race.currentCheckPoint].lines) {			
-			if (testLines(
-				vehiclesOnScreen[Player1.mot].lines[vehicleLine].p1x, 
-				vehiclesOnScreen[Player1.mot].lines[vehicleLine].p1y, 
-				vehiclesOnScreen[Player1.mot].lines[vehicleLine].p2x, 
-				vehiclesOnScreen[Player1.mot].lines[vehicleLine].p2y, 
-				
-				raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p1x, 
-				raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p1y, 
-				raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p2x, 
-				raceCheckPoints[race.currentCheckPoint].lines[checkPointLine].p2y)) {
-				
-				race.collidesCheckPoint = true;
-			}
-		}	
-	}
-}	
-
-//if (collidesSpecify( Player1.x, Player1.y, Player1.w, Player1.h, raceCheckPoints[race.currentCheckPoint].x, raceCheckPoints[race.currentCheckPoint].y, raceCheckPoints[race.currentCheckPoint].w, raceCheckPoints[race.currentCheckPoint].h ) && race.completeTimer < 1 ) {
-
-if (race.collidesCheckPoint === true) {	
-	
-	if (race.currentCheckPoint === 0) {
-		race.on = true;
-	}
-	console.log("checkpoint");
-	if (raceCheckPoints[race.currentCheckPoint].colour === "red") {
-		race.on = false;
-		race.currentCheckPoint = 0;
-		
-		
-		race.completeTimer = 60;
-	} else {
-		race.currentCheckPoint++;
-	}
-	
-	
-}
-
-//console.log(race.on);
-
-if (race.on === true) {
-	
-	race.deltaX = (raceCheckPoints[race.currentCheckPoint].x + (raceCheckPoints[race.currentCheckPoint].w / 2)) - Player1.x;
-	race.deltaY = (raceCheckPoints[race.currentCheckPoint].y + (raceCheckPoints[race.currentCheckPoint].h / 2)) - Player1.y;
-	race.angle = Math.atan2(race.deltaY,  race.deltaX);
-	race.xVector = Math.cos(race.angle);
-	race.yVector = Math.sin(race.angle);
-	
-	
-	//race.distaceToCheckPoint = Math.sqrt(Math.pow(race.xVector * 100, 2) + Math.pow(race.yVector * 100, 2) )
-	
-	race.distaceToCheckPoint = Math.sqrt(Math.pow(race.deltaX, 2) + Math.pow(race.deltaY, 2) )
-	
-	console.log (race.distaceToCheckPoint);
-	
-	if (race.distaceToCheckPoint < 150) {
-		race.indicatorAdjustor = 150 - race.distaceToCheckPoint;
-	} else {
-		race.indicatorAdjustor = 0;
-	}
-	
-	race.indicatorX = race.xVector * (150 - race.indicatorAdjustor);
-	race.indicatorY = race.yVector * (150 - race.indicatorAdjustor);
-	
-	
-	
-	c.beginPath();
-	c.save();
-	c.fillStyle = raceCheckPoints[race.currentCheckPoint].colour;
-	c.translate(Player1.x, Player1.y);
-	c.arc(race.indicatorX - 5 - cameraX, race.indicatorY - 5 - cameraY, 5, 0, 2*Math.PI);
-	c.lineWidth = 2;
-	c.stroke();
-	c.fill();
-	c.restore();
-	
-	race.currentRaceTime += 0.02; 
-}
-
-}
+updateRace();
 
 ///time
 updateTime();
